bfde89daaea8c4b2635f022f1fa52a680436012d
Michael Chung <michael.chung1994@gmail.com>
2018-12-05 19:12:12 +0000

    project for w2d2 finished

--- tinyApp/express_server.js
+++ tinyApp/express_server.js
@@ -1,28 +1,75 @@
 var express = require("express");
 var app = express();
 var PORT = 8080; // default port 8080
+const alphaNum = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+function generateRandomString(chars) {
+  var result = '';
+  for(var i = 6; i > 0; i--) {
+    result += chars[Math.floor(Math.random() * chars.length)];
+  }
+  return result;
+}
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  "9sm5xK": "http://www.google.com",
 };
 
 app.get("/", (req, res) => {
   res.send("Hello!");
-})
-.get("/urls.json", (req, res) => {
+});
+
+app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
-})
-.get("/hello", (req, res) => {
+});
+
+app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
-})
-.get("/urls", (req, res) => {
+});
+
+app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  // let longURL = ...
+  let longURL = urlDatabase[req.params.shortURL];
+
+  res.redirect(longURL);
+});
+
+app.post("/urls", (req, res) => {
+
+  let shortURL = generateRandomString(alphaNum);
+  let longURL = req.body.longURL;
+
+  // console.log(`${shortURL} - ${longURL}`);
+
+  urlDatabase[shortURL] = longURL;
+
+  let redirect = `http://localhost:8080/u/${shortURL}`;
+
+  res.redirect(redirect);
+});
+
+app.get("/urls/:id", (req, res) => {
+  let templateVars = { shortURL: req.params.id,
+                       urls: urlDatabase };
+  res.render("urls_show", templateVars);
 });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
